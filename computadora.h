0361145f20a65566d9183730c157dd668524fc03
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2020-10-11 21:36:27 -0500

    Se agregó la clase computadora con sus respectivos atributos y constructores

---
+++ computadora.h
@@ -0,0 +1,30 @@
+#ifndef COMPUTADORA_H
+#define COMPUTADORA_H
+
+#include <iostream>
+
+using namespace std;
+
+class Computadora{
+    private:
+        string nombre;
+        string SO;        
+        string procesador;
+        float RAM;
+    public:
+        Computadora();
+        Computadora(const string &nombre, const string &SO, const string &procesador, float RAM);
+
+        void setNombre(const string &n);
+        void setSO(const string &s);
+        void setProcesador(const string &p);
+        void setRAM(float r);
+
+        string getNombre();
+        string getSO();
+        string getProcesador();
+        float getRAM();
+
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "computadora.h"
+
+using namespace std;
+
+int main(){
+    cout << "Hola";
+    return 0;
+}
